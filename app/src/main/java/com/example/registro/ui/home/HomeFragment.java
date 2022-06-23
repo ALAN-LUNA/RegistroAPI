c1833ddc4453a41592e846731c211abcee2f1ffe
Alan Luna <alanluna816@gmail.com>
2022-06-23 12:17:28 -0500

    logica

--- app/src/main/java/com/example/registro/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/registro/ui/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,8 +18,11 @@ import com.example.registro.databinding.FragmentHomeBinding;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.POST;
 
 public class HomeFragment extends Fragment {
 
@@ -51,13 +55,35 @@ public class HomeFragment extends Fragment {
                 final OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
                 httpClient.addInterceptor(logging);
                 Retrofit retrofit=new Retrofit.Builder()
-                        .baseUrl("https://eyiogthd.lucusvirtual.es/")
+                        .baseUrl("https://eyiogthd.lucusvirtual.es")
                         .addConverterFactory(GsonConverterFactory.create())
                         .client(httpClient.build())
                         .build();
 
                 registerApi = retrofit.create(RegisterApi.class);
-                Call<Register> call=registerApi.getElements();
+
+                Register post = new Register("EtName","EtApPat","EtApMat","EtEmail","EtPass","EtValidate","2001-08-21");
+
+                Call<Register> call = registerApi.createpost(post);
+
+                call.enqueue(new Callback<Register>() {
+                    @Override
+                    public void onResponse(Call<Register> call, Response<Register> response) {
+                        try {
+                            if(response.isSuccessful()){
+                                Toast.makeText(getContext(), "Registrado correctamente", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }catch (Exception ex){
+                            Toast.makeText(getContext(), "no"+ ex.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Register> call, Throwable t) {
+
+                    }
+                });
             }
         });
         return root;
--- app/src/main/java/com/example/registro/ui/home/RegisterApi.java
+++ app/src/main/java/com/example/registro/ui/home/RegisterApi.java
@@ -1,11 +1,12 @@
 package com.example.registro.ui.home;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface RegisterApi {
     String API_ROUTE = "/api/registro";
 
     @POST(API_ROUTE)
-    Call<Register> getElements();
+    Call<Register> createpost(@Body Register post);
 }
